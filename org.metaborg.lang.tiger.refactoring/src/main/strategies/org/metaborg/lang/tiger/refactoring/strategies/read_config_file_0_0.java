386c3b36fa59c9ab56a62a63dc061ae3a2d72c5f
pmisteli <p.d.misteli@student.tudelft.nl>
2019-06-06 16:24:47 +0200

    Read Target Name from Config File #9
    Implement Config File Reading

--- org.metaborg.lang.tiger.refactoring/src/main/strategies/org/metaborg/lang/tiger/refactoring/strategies/read_config_file_0_0.java
+++ org.metaborg.lang.tiger.refactoring/src/main/strategies/org/metaborg/lang/tiger/refactoring/strategies/read_config_file_0_0.java
@@ -1,5 +1,16 @@
 package org.metaborg.lang.tiger.refactoring.strategies;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Properties;
+
+import org.apache.commons.vfs2.FileObject;
+import org.metaborg.core.context.IContext;
+import org.metaborg.core.resource.IResourceService;
+import org.spoofax.interpreter.core.Tools;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.ITermFactory;
 import org.strategoxt.lang.Context;
@@ -11,8 +22,36 @@ public class read_config_file_0_0 extends Strategy {
 
 	@Override
 	public IStrategoTerm invoke(Context context, IStrategoTerm current) {
-		final ITermFactory factory = context.getFactory();
-		IStrategoTerm after = factory.makeString("a");
-		return after;
+		ITermFactory factory = context.getFactory();
+		String propertyFilePath = Tools.asJavaString(current);
+		FileObject propertyFileObject = getPropertyFileObject(context, propertyFilePath);
+		String property = readProperty(propertyFileObject);
+		IStrategoTerm propertyTerm = factory.makeString(property);
+		return propertyTerm;
+	}
+
+	private FileObject getPropertyFileObject(Context context, String propertyFilePath) {
+		URI propertyFileUri;
+		try {
+			propertyFileUri = new URI(propertyFilePath);
+		} catch (URISyntaxException e) {
+			throw new IllegalArgumentException("Invalid porperty file URI: " + propertyFilePath);
+		}
+		IContext contextObject = (IContext) context.contextObject();
+		IResourceService resourceService = contextObject.injector().getInstance(IResourceService.class);
+		FileObject fileObject = resourceService.resolve(propertyFileUri);
+		return fileObject;
+	}
+
+	private String readProperty(FileObject fileObject) {
+		try (InputStream input = fileObject.getContent().getInputStream()) {
+			Properties prop = new Properties();
+			prop.load(input);
+
+			return prop.getProperty("refactoring.rename");
+
+		} catch (IOException ex) {
+			throw new IllegalArgumentException("Invalid porperty file URI: " + fileObject.getPublicURIString());
+		}
 	}
 }

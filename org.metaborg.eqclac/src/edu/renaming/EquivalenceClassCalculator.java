3dbe1786f4badb3b34326ad9457ba573ed177015
pmisteliac <p.d.misteli@student.tudelft.nl>
2020-02-22 11:09:32 +0100

    Add Java project to repo to implement equivalence class calculator #36

---
+++ org.metaborg.eqclac/src/edu/renaming/EquivalenceClassCalculator.java
@@ -0,0 +1,14 @@
+package edu.renaming;
+
+public final class EquivalenceClassCalculator {
+	
+	private EquivalenceClassCalculator() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ org.metaborg.eqclac/src/edu/renaming/NameIndex.java
@@ -0,0 +1,40 @@
+package edu.renaming;
+
+public final class NameIndex {
+	
+	private final int numIndex;
+	private final String fileName;
+	
+	public NameIndex(int numIndex, String fileName) {
+		this.numIndex = numIndex;
+		this.fileName = fileName;
+	}
+
+	public int getNumIndex() {
+		return numIndex;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (! (obj instanceof NameIndex)) {
+			return false;
+		}
+		NameIndex other = (NameIndex) obj;
+		
+		if(other.getNumIndex() != this.numIndex) {
+			return false;
+		}
+		
+		if(!other.getFileName().equals(this.fileName)) {
+			return false;
+		}
+		
+		return true;
+	}
+
+}

d7367ed685bf7107096c56d35f1fd63f0906108d
pmisteliac <p.d.misteli@student.tudelft.nl>
2020-02-24 11:00:39 +0100

    Add implentation for calculating reflexive & symetric closure

--- org.metaborg.eqclac/src/main/org/metaborg/eqclac/EquivalenceClassCalculator.java
+++ org.metaborg.eqclac/src/main/org/metaborg/eqclac/EquivalenceClassCalculator.java
@@ -2,6 +2,7 @@ package org.metaborg.eqclac;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public final class EquivalenceClassCalculator {
 	
@@ -14,9 +15,15 @@ public final class EquivalenceClassCalculator {
 	}
 	
 	public static void calculate(Set<ResolutionPair> resolutionRelation) {
-		//calculate reflexive closure
-		//calculate symetric closure
+		Set<NameIndex> nameIndexes = gatherNameIndexes(resolutionRelation);
+		Set<ResolutionPair> reflexiveClosure = calcReflexiveClosure(nameIndexes);
+		Set<ResolutionPair> symetricClosure = calcSymetricClosure(resolutionRelation);
 		//calculate transitive closure
+		
+		Set<ResolutionPair> equivalenceRelation = new HashSet<>();
+		equivalenceRelation.addAll(resolutionRelation);
+		equivalenceRelation.addAll(reflexiveClosure);
+		equivalenceRelation.addAll(symetricClosure);
 		//calculate equivalence classes
 	}
 	
@@ -27,6 +34,21 @@ public final class EquivalenceClassCalculator {
 			indexes.add(pair.getReference());
 		}
 		return indexes;
+	}
+	
+	public static Set<ResolutionPair> calcReflexiveClosure(Set<NameIndex> nameIndexes) {
+		Set<ResolutionPair> closure = nameIndexes.stream()
+									  .map(index -> new ResolutionPair(index, index))
+									  .collect(Collectors.toSet());
+		return closure;
+		
+	}
+	
+	public static Set<ResolutionPair> calcSymetricClosure(Set<ResolutionPair> resolutionRelation) {
+		Set<ResolutionPair> closure = resolutionRelation.stream()
+									  .map(pair -> new ResolutionPair(pair.getDeclaration(), pair.getReference()))
+									  .collect(Collectors.toSet());
+		return closure;
 	}
 
 }

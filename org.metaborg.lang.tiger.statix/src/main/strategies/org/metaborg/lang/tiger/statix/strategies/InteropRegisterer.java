18fffb84e06cfba4900e652a331938e3324829ed
pmisteliac <p.d.misteli@student.tudelft.nl>
2020-03-11 19:00:19 +0100

    Migrate renaming to Statix #32

--- org.metaborg.lang.tiger.statix/src/main/strategies/org/metaborg/lang/tiger/statix/strategies/InteropRegisterer.java
+++ org.metaborg.lang.tiger.statix/src/main/strategies/org/metaborg/lang/tiger/statix/strategies/InteropRegisterer.java
@@ -4,7 +4,7 @@ import org.strategoxt.lang.JavaInteropRegisterer;
 import org.strategoxt.lang.Strategy;
 
 public class InteropRegisterer extends JavaInteropRegisterer {
-    public InteropRegisterer() {
-        super(new Strategy[] { });
-    }
+	public InteropRegisterer() {
+		super(new Strategy[] { calc_name_cluster_0_0.instance });
+	}
 }

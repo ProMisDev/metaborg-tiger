91ff580c020fc17f24c63a5530c9a678bdcdda43
pmisteliac <p.d.misteli@student.tudelft.nl>
2020-12-10 07:22:56 +0100

    Remove refactoring implementation and switch to integrated one

--- org.metaborg.lang.tiger.refactoring/src/main/strategies/org/metaborg/lang/tiger/refactoring/strategies/InteropRegisterer.java
+++ org.metaborg.lang.tiger.refactoring/src/main/strategies/org/metaborg/lang/tiger/refactoring/strategies/InteropRegisterer.java
@@ -1,10 +1,8 @@
 package org.metaborg.lang.tiger.refactoring.strategies;
 
 import org.strategoxt.lang.JavaInteropRegisterer;
-import org.strategoxt.lang.Strategy;
 
 public class InteropRegisterer extends JavaInteropRegisterer {
 	public InteropRegisterer() {
-		super(new Strategy[] { calc_name_cluster_0_0.instance });
 	}
 }
